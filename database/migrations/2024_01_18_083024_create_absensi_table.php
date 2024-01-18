31ce86724698e20f302c9cbf5ea8b6f8167667f4
mrizkystr <muhamadrizkysatria23@gmail.com>
2024-01-18 15:35:13 +0700

    ini update kedua

---
+++ database/migrations/2024_01_18_083024_create_absensi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('class');
+            $table->string('departement');
+            $table->enum('attedance',['hadir', 'izin'. 'sakit', 'alfa']);
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensi');
+    }
+};
---
+++ database/migrations/2024_01_18_083143_create_surat_izin_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surat_izin', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('class');
+            $table->string('departement');
+            $table->string('permission');
+            $table->string('reason');
+            $table->dateTime('date_submission')->nullable()->default(now());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surat_izin');
+    }
+};
